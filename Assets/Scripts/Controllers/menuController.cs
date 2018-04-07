320eaa7348828fe90b975794baf644467c88e98f
Ethan Rockwood <drockwood94@gmail.com>
2018-04-06 21:29:03 -0500

    Set up initial unity project for Life of an Asset

---
+++ Assets/Scripts/Controllers/menuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour {
+
+    public Text statusText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonEnter(Text buttonText)
+    {
+        statusText.text = "Hovering on " + buttonText.text;
+    }
+    public void OnButtonExit()
+    {
+        statusText.text = "";
+    }
+}
